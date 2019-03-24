b6fae58b3a678c17a40f48dd2cf926a6cc17de43
1Conan <me@1conan.com>
2019-03-24 19:42:30 +0900

    serializers: bigint - typescript rewrite

--- serializers/bigint.ts
+++ serializers/bigint.ts
@@ -1,22 +1,21 @@
 // Copyright (c) 2017-2019 dirigeants. All rights reserved. MIT license.
 /* global BigInt:false */
 
-const { Serializer } = require('klasa');
+import { Language, SchemaPiece, Serializer } from 'klasa';
 
-module.exports = class extends Serializer {
+export default class extends Serializer {
 
-	deserialize(data, piece, language) {
+	async deserialize(data: any, piece: SchemaPiece, language: Language) {
 		if (data instanceof BigInt) return data;
 		try {
-			// eslint-disable-next-line new-cap
 			return BigInt(data);
 		} catch (err) {
 			throw language.get('RESOLVER_INVALID_INT', piece.key);
 		}
 	}
 
-	serialize(data) {
+	serialize(data: any) {
 		return data.toString();
 	}
 
-};
+}
