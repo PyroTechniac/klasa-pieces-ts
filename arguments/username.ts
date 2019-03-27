106e1e79815095f5dde39b542a6808addd2c4e23
1Conan <me@1conan.com>
2019-03-27 20:26:10 +0900

    argument: username - typescript rewrite

--- arguments/username.ts
+++ arguments/username.ts
@@ -1,39 +1,39 @@
 // Copyright (c) 2017-2019 dirigeants. All rights reserved. MIT license.
-const { Argument, util: { regExpEsc } } = require('klasa');
-const { GuildMember, User } = require('discord.js');
+import { Guild, GuildMember, User,  } from 'discord.js';
+import { Argument, KlasaMessage, Possible, util } from 'klasa';
 
 const USER_REGEXP = Argument.regex.userOrMember;
 
-function resolveUser(query, guild) {
+function resolveUser(query: GuildMember | User | string, guild: Guild) {
 	if (query instanceof GuildMember) return query.user;
 	if (query instanceof User) return query;
 	if (typeof query === 'string') {
-		if (USER_REGEXP.test(query)) return guild.client.users.fetch(USER_REGEXP.exec(query)[1]).catch(() => null);
+		if (USER_REGEXP.test(query)) return guild.client.users.fetch((USER_REGEXP.exec(query) as RegExpExecArray)[1]).catch(() => null);
 		if (/\w{1,32}#\d{4}/.test(query)) {
-			const res = guild.members.find(member => member.user.tag === query);
+			const res = guild.members.find((member: GuildMember) => member.user.tag === query);
 			return res ? res.user : null;
 		}
 	}
 	return null;
 }
 
-module.exports = class extends Argument {
+export default class extends Argument {
 
-	async run(arg, possible, msg) {
+	async run(arg: string, possible: Possible, msg: KlasaMessage): Promise<User> {
 		if (!msg.guild) return this.store.get('user').run(arg, possible, msg);
 		const resUser = await resolveUser(arg, msg.guild);
 		if (resUser) return resUser;
 
 		const results = [];
-		const reg = new RegExp(regExpEsc(arg), 'i');
+		const reg = new RegExp(util.regExpEsc(arg), 'i');
 		for (const member of msg.guild.members.values()) {
 			if (reg.test(member.user.username)) results.push(member.user);
 		}
 
 		let querySearch;
 		if (results.length > 0) {
-			const regWord = new RegExp(`\\b${regExpEsc(arg)}\\b`, 'i');
-			const filtered = results.filter(user => regWord.test(user.username));
+			const regWord = new RegExp(`\\b${util.regExpEsc(arg)}\\b`, 'i');
+			const filtered = results.filter((user) => regWord.test(user.username));
 			querySearch = filtered.length > 0 ? filtered : results;
 		} else {
 			querySearch = results;
@@ -42,8 +42,8 @@ module.exports = class extends Argument {
 		switch (querySearch.length) {
 			case 0: throw `${possible.name} Must be a valid name, id or user mention`;
 			case 1: return querySearch[0];
-			default: throw `Found multiple matches: \`${querySearch.map(user => user.tag).join('`, `')}\``;
+			default: throw `Found multiple matches: \`${querySearch.map((user) => user.tag).join('`, `')}\``;
 		}
 	}
 
-};
+}
