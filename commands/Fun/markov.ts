81c82e0a017aabaa1079d5e84401264e92817f0c
1Conan <me@1conan.com>
2019-10-31 15:05:31 +0900

    src: cmd/fun/markov rewrite

--- commands/Fun/markov.ts
+++ commands/Fun/markov.ts
@@ -1,21 +1,21 @@
 // Copyright (c) 2017-2019 dirigeants. All rights reserved. MIT license.
-const { Command } = require('klasa');
+import { KlasaClient, KlasaMessage, Command, CommandStore } from 'klasa';
 const MarkovChain = require('markovchain');
 const messageLimitHundreds = 1;
 
-module.exports = class extends Command {
+export default class extends Command {
 
-	constructor(...args) {
-		super(...args, {
+	constructor(client: KlasaClient, store: CommandStore, file: string[], dir: string) {
+		super(client, store, file, dir, {
 			description: 'Generate a markov chain from the text chat.',
 			requiredPermissions: ['READ_MESSAGE_HISTORY']
 		});
 	}
 
-	async run(msg) {
+	async run(msg: KlasaMessage) {
 		let messageBank = await msg.channel.messages.fetch({ limit: 100 });
 		for (let i = 1; i < messageLimitHundreds; i++) {
-			messageBank = messageBank.concat(await msg.channel.messages.fetch({ limit: 100, before: messageBank.last().id }));
+			messageBank = messageBank.concat(await msg.channel.messages.fetch({ limit: 100, before: messageBank.last()!.id }));
 		}
 
 		const quotes = new MarkovChain(messageBank.map(message => message.content).join(' '));
@@ -23,9 +23,9 @@ module.exports = class extends Command {
 		return msg.sendMessage(chain.substring(0, 1999));
 	}
 
-	useUpperCase(wordList) {
+	useUpperCase(wordList: any) {
 		const tmpList = Object.keys(wordList).filter((word) => word[0] >= 'A' && word[0] <= 'Z');
 		return tmpList[Math.floor(Math.random() * tmpList.length)];
 	}
 
-};
+}
